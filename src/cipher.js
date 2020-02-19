56d5e13c063d02330570109ddc687f2743de04aa
verenisseeyzaguirre <verenisse.eyzaguirre@gmail.com>
2020-02-19 08:29:22 -0500

    solo 3 de 7 test funcionan

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,25 @@
-const cipher = {
-  // ...
-};
+let mensaje;
+let letra;
+let texto='';
 
-export default cipher;
+const cipher = {
+  encode(offset, string){
+  for(var i = 0; i < string.length; i+=1){
+    letra = String.fromCharCode((string.charCodeAt(i)-65+offset)%26 + 65);
+    texto = texto+letra;
+    } 
+    mensaje = texto;
+    texto='';
+    return(mensaje);
+},
+  decode(offset, string){
+  for(var i = 0; i < string.length; i+=1){
+    //letra = String.fromCharCode(string.charCodeAt(i)-(offset)%26);
+    letra = String.fromCharCode( string.charCodeAt(i)-(string.charCodeAt(i)-65+offset)%26);
+    texto = texto+letra;
+    } 
+    mensaje = texto;
+    texto='';
+    return(mensaje);
+}
+};export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,122 +1,58 @@
 import cipher from './cipher.js';
+//console.log(cipher);
+let string;
+let offset;
 
-console.log(cipher);
-
-console.log('hola')
-
-let alfabeto =["A","B","C","D","E"];
-
-
-const numero = document.getElementById('saltos').value;
-console.log(numero);
-//const parrafo = document.getElementById('saludo');
-//parrafo.innerHTML= name;
-
-const imprimirAlf= document.getElementById('alfabeto');
-
-
-imprimirAlf.innerHTML= alfabeto[1];	
-
-for(var i = 0; i < alfabeto.length; i++){
-  console.log("x");
-}
-
-
-document.getElementById('resultado').style.display = 'none';
-// se cambió a funcion arrow const square = num => num * num;
-const ocultar = () => {
-    document.getElementById('entrada').style.display = 'none';
+//funciones arrow ejm: const square = num => num * num;
+const ocultarEntrada = () => {
     document.getElementById('botones').style.display = 'none';
     }
 
-const mostrar = () => {
-    document.getElementById('entrada').style.display = 'block';
+const mostrarEntrada = () => {
     document.getElementById('botones').style.display = 'block';
     }
 
-const mostrarRESULTADO = () => {
-    document.getElementById('resultado').style.display = 'block';
-    //captura elementos del HTML
-    let mensajeOriginal = document.getElementById('entrada').value;
-    console.log(mensajeOriginal);
-    //captura elementos del HTML
-    let desplazamientos = document.getElementById('saltos').value;
-    console.log(desplazamientos);
-    }    
-    
-const ocultarRESULTADO = () => {
+const ocultarResultado = () => {
     document.getElementById('resultado').style.display = 'none';
     }
+   
+const limpiarEjercicio = () => {
+    //sirve para borrar los datos ingresados y almacenados anteriormente
+    document.getElementById('entrada').value= '';
+    document.getElementById('saltos').value= '';
+    }
 
+//buscando unir capturarDatosA y capturarDatosB , A es 65 y Z a 90
+    const capturarDatosA = () => {
+      string = document.getElementById('entrada').value;
+      offset = parseInt(document.getElementById('saltos').value);
+      imprimiendoMensaje(cipher.encode(offset, string));
+  }  
+
+    const capturarDatosB = () => {
+    string = document.getElementById('entrada').value;
+    offset = parseInt(document.getElementById('saltos').value);
+    imprimiendoMensaje(cipher.decode(offset, string));
+} 
+
+const imprimiendoMensaje = (nuevoMensaje) => {
+    const mensajeResuelto = document.getElementById('pantalla');
+    mensajeResuelto.innerHTML= nuevoMensaje;
+    document.getElementById('resultado').style.display = 'block';
+} 
+
+//llama al boton cifrar y luego ejecuta las funciones , oculta la primera pantalla y muestra el resultado cifrado
 const botonCifrar = document.getElementById('btnCifrar');
-botonCifrar.addEventListener('click', ocultar);
-botonCifrar.addEventListener('click', mostrarRESULTADO);
+botonCifrar.addEventListener('click', ocultarEntrada);
+botonCifrar.addEventListener('click', capturarDatosA);
 
+//llaman al boton descifrar y luego ejecuta las funciones, oculta la primera pantalla y muestra el resultado descifrado
 const botonDescifrar = document.getElementById('btnDescifrar');
-botonDescifrar.addEventListener('click', ocultar);
-botonDescifrar.addEventListener('click', mostrarRESULTADO);
+botonDescifrar.addEventListener('click', ocultarEntrada);
+botonDescifrar.addEventListener('click', capturarDatosB);
 
-
-
+//llama al boton Volver a intentar, oculta los resultados muestra la primera pantalla limpia los datos anteriores
 const botonVolverIntentar = document.getElementById('btnVolverIntentar');
-botonVolverIntentar.addEventListener('click', mostrar);
-botonVolverIntentar.addEventListener('click', ocultarRESULTADO);
-
-
-
-//para revisar
-
-
-
-let saltos = desplazamientos;
-let alfaSinCifrar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-let alfaCifrado =[];
-
-
-for (var i = saltos, j = 0; i < alfaSinCifrar.length; i+=1) {
-  //console.log("En el índice del array principal '" + i + "' hay este valor: " + alfaSinCifrar[i]);
-  let dato = alfaSinCifrar[i];
-  //console.log(dato);
-  alfaCifrado.push(dato);
-  //console.log("En el índice del array secundario '" + j + "' hay este valor: " + alfaCifrado[j]);
-  
-}
-
-for (var i = 0; i < saltos; i+=1) {
-  //console.log("En el índice del array principal '" + i + "' hay este valor: " + alfaSinCifrar[i]);
-  let dato = alfaSinCifrar[i];
-  //console.log(dato);
-  alfaCifrado.push(dato);
-  //console.log("En el índice del array secundario '" + j + "' hay este valor: " + alfaCifrado[j]);
-  
-}
-
-alfaCifrado.forEach(function (valor) {
-  document.write(valor + ' ');
-  });
-
-//alfaSinCifrar.forEach(function (valor) {
-//document.write(valor + ' ');
-//});  
-
-
-let mensaje = mensajeOriginal;
-let strMensaje = mensaje.split('');
-let nuevoMensaje =[];
-
-  
-
-  for(let i = 0; i < strMensaje.length; i+=1){
-    let letra = strMensaje[i];
-    //console.log("debe ser la C" );
-    //console.log(letra);
-    let indice = alfaSinCifrar.indexOf(letra);
-    //console.log("debe ser la ubicación 2" );
-    //console.log(indice);
-    let nuevo = alfaCifrado[indice];
-    //console.log("lo q reemplaza a C" );
-    //console.log(nuevo);
-    nuevoMensaje.push(nuevo)
-  }
-
-  let strNuevoMensaje = nuevoMensaje.join('');
+botonVolverIntentar.addEventListener('click', ocultarResultado);
+botonVolverIntentar.addEventListener('click', mostrarEntrada);
+botonVolverIntentar.addEventListener('click', limpiarEjercicio);
